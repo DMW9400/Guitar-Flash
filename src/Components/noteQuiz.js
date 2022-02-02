e79f8e2b0b78cb9b3fb650a17ef6b91c42d910a9
Matthew Welch <dmw9400@gmail.com>
2022-02-01 23:47:47 -0500

    working on checkbox logic

--- src/Components/noteQuiz.js
+++ src/Components/noteQuiz.js
@@ -9,24 +9,61 @@ class NoteQuiz extends React.Component {
             guitarStrings:[]
         }
     }
-    // state = {
-    //     pitchClasses: [],
-    //     guitarStrings: []
-    // }
+    
+
+    handleStringChange = (event) => {
+        const checkedBool = event.target.checked
+        const selectedValue = event.target.value
+        // console.log(checkedBool, selectedValue)
+        if(checkedBool){
+            console.log('Checked')
+            // this.setState(prevState => ({
+            //     guitarStrings: [...prevState.guitarStrings, 'ABC']
+            //     }, ()=>console.log(this.state))
+            // )
+            this.setState({
+                guitarStrings: [...this.state.guitarStrings, selectedValue]
+            }, () => console.log(this.state.guitarStrings))
+        } 
+        else {
+            this.setState({
+                guitarStrings : [...this.state.guitarStrings, this.state.guitarStrings.filter(word => word !==selectedValue )]
+            }, () => console.log(this.state.guitarStrings))
+        }
+        //     this.setState(prevState => ({
+        //         [event.target.name]: prevState.event.target.name.filter(value => value !== selectedValue)
+        //         }, ()=>console.log(this.state))
+        //     )}
+        }
+
     generateCheckBox = (arr) => { 
         return (arr.map (guitString =>{
             return(
-                <h1>{guitString}</h1>
+                <div>
+                <input 
+                type = "checkbox"
+                id = {guitString}
+                key ={guitString}
+                value = {guitString}
+                name = 'guitarStrings'
+                onChange={this.handleStringChange}
+                />
+                <label>{guitString}</label>
+                </div>
             )
-        })
+        },
+        <div>
+            <input type="checkbox" />
+            <label>Select All</label>
+        </div>
+        )
         )
     }
 
     render(){
-        console.log('CALLED BABY')
         return(
         <div>
-            {this.generateCheckBox(['A','B'])}
+            {this.generateCheckBox(['E','A','D','G','B','e'])}
         </div>
         )
     }
